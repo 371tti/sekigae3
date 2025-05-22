4cb3a810978de9bef787f7279c43da5953b44862
371tti <109227273+371tti@users.noreply.github.com>
2025-05-23 00:53:48 +0900

    README.mdを追加し、座席最適化システムの概要とAPI仕様を記述

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ use std::sync::Arc;
 
 use dashmap::DashMap;
 use kurosabi::Kurosabi;
-use rand::{distributions::Alphanumeric, Rng};
-use rand::rngs::OsRng;
 use sekigae3::collector::Sekigae;
 
 struct SekigaeContext {
@@ -24,8 +22,24 @@ fn main() {
     let arc_context = Arc::new(SekigaeContext::new());
     let mut kurosabi = Kurosabi::with_context(arc_context);
 
-    kurosabi.get("/:id", |mut c| async move {
+    
+    kurosabi.get("/",  |mut c| async move {
+        c.res.text("Hello, World!");
+        let key = "session_id";
+        let value = "123456";
+        c.res.header.set_cookie(key, value);
+        c.res.header.set("X-Custom-Header", "MyValue");
+        c
+    });
+
+    kurosabi.get("/api/:id/info", |mut c| async move {
         let id = c.req.path.get_field("id").unwrap_or("".to_string());
-        let sekigae = c.c.sekigae_sessions.get_mut(&id);
-    })
+        {
+            // `c.c.sekigae_sessions` を借用するスコープを限定
+            let sekigae = c.c.sekigae_sessions.get_mut(&id);
+            // 必要な処理をここで行う
+        }
+        // 借用が解除された後に `c` を返す
+        c
+    });
 }
